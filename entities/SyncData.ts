6120f8649d62350daa871f2c1cf7f1f434a9a3d7
João Pedro <nmjoaopedro22@gmail.com>
2023-11-28 11:18:28 -0300

    fix(api/log/sync): correção para suporte multi-serviços

--- entities/SyncData.ts
+++ entities/SyncData.ts
@@ -1,17 +1,32 @@
+export type Services = "yt-music" | "spotify";
+
+export interface MediaData {
+	id: string;
+	service: Services;
+	title: string;
+	artist: string;
+	album?: string;
+	duration?: number;
+	url: string;
+}
+
 export interface SyncData {
 	id: string;
 	partyId: string;
-	serviceId: "yt-music" | "spotify";
 	device: {
 		id: string;
 		name: string;
 	};
-	media: {
+	media: MediaData[];
+}
+
+export interface SingleSyncData {
+	id: string;
+	partyId: string;
+	serviceId: Services;
+	device: {
 		id: string;
-		title: string;
-		artist: string;
-		album?: string;
-		duration?: number;
-		url: string;
+		name: string;
 	};
+	media: MediaData;
 }
--- pages/api/log/sync.ts
+++ pages/api/log/sync.ts
@@ -1,4 +1,4 @@
-import { SyncData } from "@/entities/SyncData";
+import { MediaData, Services, SingleSyncData, SyncData } from "@/entities/SyncData";
 import { realtime } from "@/models/database";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { createRouter } from "next-connect";
@@ -9,11 +9,11 @@ const router = createRouter<NextApiRequest, NextApiResponse>();
 
 interface PostHandlerBody {
 	party: string;
-	serviceId: "yt-music" | "spotify";
 	deviceId: string;
 	deviceName: string;
 	media: {
 		id: string;
+		service: Services;
 		title: string;
 		artist: string;
 		album?: string;
@@ -47,12 +47,25 @@ router
 			const docs: SyncData[] = [];
 			Object.keys(data).forEach(key => docs.push(data[key]));
 
-			const ytData: SyncData[] = [];
-			const spotifyData: SyncData[] = [];
+			const ytData: SingleSyncData[] = [];
+			const spotifyData: SingleSyncData[] = [];
 
 			for (const doc of docs) {
-				if (doc.serviceId === "yt-music") ytData.push(doc);
-				else if (doc.serviceId === "spotify") spotifyData.push(doc);
+				const media: MediaData[] = [];
+				Object.keys(doc.media).forEach(key => {
+					// @ts-ignore
+					media.push(doc.media[key]);
+				});
+
+				const hasYtMusicMedia = media.find(media => media.service === "yt-music");
+				if (hasYtMusicMedia) {
+					ytData.push({ ...doc, serviceId: "yt-music", media: hasYtMusicMedia });
+				}
+
+				const hasSpotifyMedia = media.find(media => media.service === "spotify");
+				if (hasSpotifyMedia) {
+					spotifyData.push({ ...doc, serviceId: "spotify", media: hasSpotifyMedia });
+				}
 			}
 
 			return res.status(200).json({
@@ -90,12 +103,11 @@ router
 		const syncData: SyncData = {
 			id: uuid(),
 			partyId: body.party,
-			serviceId: body.serviceId,
 			device: {
 				id: body.deviceId,
 				name: body.deviceName,
 			},
-			media: body.media,
+			media: [body.media],
 		};
 
 		try {
@@ -105,11 +117,30 @@ router
 			if (snapshot.exists() && snapshot.toJSON()) {
 				const data = snapshot.toJSON() as SyncData;
 
+				let media: MediaData[] = [];
+				Object.keys(data.media).forEach(key => {
+					// @ts-ignore
+					media.push(data.media[key]);
+				});
+
+				const hasServiceMedia = media.find(media => media.service === body.media.service);
+
+				if (hasServiceMedia) {
+					media = [
+						...media.filter(media => media.service !== body.media.service),
+						body.media,
+					];
+				} else {
+					media.push(body.media);
+				}
+
 				// @ts-ignore
 				delete syncData.id;
 				await ref.child(data.device.id).set({
-					...data,
-					...syncData,
+					id: data.id,
+					partyId: syncData.partyId || data.partyId,
+					device: syncData.device || data.device,
+					media: media,
 				});
 			} else {
 				await ref.child(syncData.device.id).set(syncData);
